0fbeed376b486d4a94e2fae68655ca9f9d69efbf
akim-dev <78294519+akim-dev@users.noreply.github.com>
2023-05-11 22:57:52 +0700

    1 file modified

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,16 +3,21 @@ import React from 'react';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2020, 2, 20);
-  const expenseTitle = 'Car insurance';
-  const expenseAmout = 326.23;
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
 
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">$ {expenseAmout}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
       </div>
       {console.info(props)}
     </div>
