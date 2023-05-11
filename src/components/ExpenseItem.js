e356488ac935fdd7eb99304ffaf8fcc683d77a22
akim-dev <78294519+akim-dev@users.noreply.github.com>
2023-05-12 00:26:46 +0700

    2 files modified

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
 import './ExpenseItem.css';
-
-import ExpsenseDate from './ExpenseDate';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
+      <ExpenseDate hari={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
